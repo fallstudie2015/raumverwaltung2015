e7b7caefe0643fb15de5c2eb7a2d0ad383014995
Kai <SCCplayer@gmx.de>
2015-10-16 13:07:37 +0200

    Raumklasse vervollständigt

--- src/standardklassen/Raum.java
+++ src/standardklassen/Raum.java
@@ -1,10 +1,59 @@
 package standardklassen;
 
 public class Raum {
-	
 	private int raumID;
 	private String name;
 	private String strasse;
 	private String stock;
 	private int anzPersonen;
+
+	public Raum(int raumID, String name, String strasse, String stock,
+			int anzPersonen) {
+		this.raumID = raumID;
+		this.name = name;
+		this.strasse = strasse;
+		this.stock = stock;
+		this.anzPersonen = anzPersonen;
+	}
+
+	public int getRaumID() {
+		return raumID;
+	}
+
+	public void setRaumID(int raumID) {
+		this.raumID = raumID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getStrasse() {
+		return strasse;
+	}
+
+	public void setStrasse(String strasse) {
+		this.strasse = strasse;
+	}
+
+	public String getStock() {
+		return stock;
+	}
+
+	public void setStock(String stock) {
+		this.stock = stock;
+	}
+
+	public int getAnzPersonen() {
+		return anzPersonen;
+	}
+
+	public void setAnzPersonen(int anzPersonen) {
+		this.anzPersonen = anzPersonen;
+	}
+
 }
