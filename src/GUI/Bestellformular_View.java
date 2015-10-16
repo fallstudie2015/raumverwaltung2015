1d60b830bdce82a9f1243f7c29ab1d0add427250
Tim <tim.deisser@gmail.com>
2015-10-16 14:50:57 +0200

    obere Hälfte des SplitPanes

--- src/GUI/Bestellformular_View.java
+++ src/GUI/Bestellformular_View.java
@@ -1,9 +1,14 @@
 package GUI;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JSplitPane;
 import javax.swing.JTextArea;
@@ -11,15 +16,20 @@ import javax.swing.JTextField;
 
 public class Bestellformular_View extends JSplitPane {
 
-	private JLabel raumLabel, namelabel, bereichLabel, telLabel, DatumLabel, zeitVonLabel, zeitBisLabel, personenLabel,
-			technikLabel, bestuhlungLabel;
+	private JLabel raumLabel, namelabel, bereichLabel, telLabel, DatumLabel,
+			zeitVonLabel, zeitBisLabel, personenLabel, technikLabel,
+			bestuhlungLabel;
 	private JTextField telField, personenField;
-	private JComboBox<Integer> datumTagCB, datumMonatCB, datumJahrCB, zeitVonStundeCB, zeitVonMinuteCB,
-			zeitBisStundeCB, zeitBisMinuteCB;
+	private JComboBox datumTagCB, datumMonatCB, datumJahrCB, zeitVonStundeCB,
+			zeitVonMinuteCB, zeitBisStundeCB, zeitBisMinuteCB;
 	private JComboBox<String> bestuhlungCB, ausstattungCB;
 	private JRadioButton internRB, externRB;
 	private JButton vormerkenButton, abbrechenButton, addButton;
 	private JTextArea sonstigeArea;
+	private String stunde[] = { "8", "9", "10", "11", "12", "13", "14", "15",
+			"16", "17", "18", "19" };
+	private String minute[] = { "00", "05", "10", "15", "20", "25", "30", "35",
+			"40", "45", "50", "55" };
 
 	public Bestellformular_View() {
 		initView();
@@ -29,4 +39,98 @@ public class Bestellformular_View extends JSplitPane {
 
 	}
 
+	private JPanel raumUndNamePanel() {
+		raumLabel = new JLabel();
+		raumLabel.setPreferredSize(new Dimension(100, 30));
+
+		namelabel = new JLabel();
+		namelabel.setPreferredSize(new Dimension(50, 30));
+
+		bereichLabel = new JLabel();
+		bereichLabel.setPreferredSize(new Dimension(50, 30));
+
+		JPanel nameBereichPanel = new JPanel();
+		nameBereichPanel.setLayout(new GridLayout(1, 2));
+
+		nameBereichPanel.add(namelabel);
+		nameBereichPanel.add(bereichLabel);
+
+		JPanel raumNamePanel = new JPanel();
+		raumNamePanel.setLayout(new GridLayout(2, 1));
+
+		raumNamePanel.add(raumLabel);
+		raumNamePanel.add(nameBereichPanel);
+
+		return raumNamePanel;
+	}
+
+	private JPanel telPanel() {
+		telLabel = new JLabel();
+		telLabel.setPreferredSize(new Dimension(50, 30));
+
+		telField = new JTextField();
+		telField.setPreferredSize(new Dimension(100, 30));
+
+		JPanel telPanel = new JPanel();
+		telPanel.setLayout(new GridLayout(1, 2));
+
+		telPanel.add(telLabel);
+		telPanel.add(telField);
+
+		return telPanel;
+	}
+
+	private JPanel datumPanel() {
+
+	}
+
+	private JPanel zeitVonPanel() {
+		zeitVonLabel = new JLabel("Zeit von:");
+		zeitVonLabel.setPreferredSize(new Dimension(100, 30));
+
+		zeitVonStundeCB = new JComboBox(stunde);
+		zeitVonMinuteCB = new JComboBox(minute);
+
+		JPanel minUndStdPanel = new JPanel();
+		minUndStdPanel.setLayout(new GridLayout(1, 2));
+
+		minUndStdPanel.add(zeitVonStundeCB);
+		minUndStdPanel.add(zeitVonMinuteCB);
+
+		JPanel zeitLabelPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		zeitLabelPanel.add(zeitVonLabel);
+
+		JPanel zeitVonPanel = new JPanel();
+		zeitVonPanel.setLayout(new BorderLayout());
+
+		zeitVonPanel.add(zeitLabelPanel);
+		zeitVonPanel.add(zeitLabelPanel);
+
+		return zeitVonPanel;
+	}
+
+	private JPanel zeitBisPanel() {
+		zeitBisLabel = new JLabel("bis:");
+		zeitBisLabel.setPreferredSize(new Dimension(100, 30));
+
+		zeitBisStundeCB = new JComboBox(stunde);
+		zeitBisMinuteCB = new JComboBox(minute);
+
+		JPanel minUndStdPanel = new JPanel();
+		minUndStdPanel.setLayout(new GridLayout(1, 2));
+
+		minUndStdPanel.add(zeitBisStundeCB);
+		minUndStdPanel.add(zeitBisMinuteCB);
+
+		JPanel zeitLabelPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		zeitLabelPanel.add(zeitBisLabel);
+
+		JPanel zeitBisPanel = new JPanel();
+		zeitBisPanel.setLayout(new BorderLayout());
+
+		zeitBisPanel.add(zeitLabelPanel);
+		zeitBisPanel.add(zeitLabelPanel);
+
+		return zeitBisPanel;
+	}
 }
