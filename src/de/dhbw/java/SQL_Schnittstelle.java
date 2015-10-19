caac313ca9620c80946e0888fef4e16f6ce670a4
Kai <SCCplayer@gmx.de>
2015-10-19 14:27:11 +0200

    Main einstiegspunkt erstellt

--- src/de/dhbw/java/SQL_Schnittstelle.java
+++ src/de/dhbw/java/SQL_Schnittstelle.java
@@ -7,48 +7,49 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class SQL_Schnittstelle {
-	
+
 	// methode coinncetionaufbauen
 	// methode connection schließen
 	// methode batch verarbeitung
 	// methode einzelne query
-	static Connection con; 		// Verbindung zur Datenbank
+	static Connection con; // Verbindung zur Datenbank
 
-	public SQL_Schnittstelle()
-	{
-	try{
-	// Datenbanktreiber fuer MySQL laden
-		Class.forName("com.mysql.jdbc.Driver");
-		System.out.println("Treiber gefunden\n--------------------------------");
+	public static void createConnection() {
+		try {
+			// Datenbanktreiber fuer MySQL laden
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out
+					.println("Treiber gefunden\n--------------------------------");
+		} catch (ClassNotFoundException e) {
+			System.out.println("Treiber nicht gefunden");
+		}
+		// Connection zur Datenbank aufbauen
+		try {
+			con = DriverManager.getConnection(
+					"jdbc:mysql://sql2.freemysqlhosting.net:3306/sql293125",
+					"sql293125", "dG2!gP3!");
+		} catch (SQLException s) {
+			System.out.println("SQL-Exception aufgetreten");
+		}
 	}
-	catch(ClassNotFoundException e){
-		System.out.println("Treiber nicht gefunden");
-	}
-	// Connection zur Datenbank aufbauen
-	try{
-		con=DriverManager.getConnection("jdbc:mysql://sql2.freemysqlhosting.net:3306/sql293125","sql293125","dG2!gP3!");}
-	catch(SQLException s){System.out.println("SQL-Exception aufgetreten");}
-	} /* end Konstruktor */
 
-	public static ResultSet sqlAbfrage(String abfrage)
-	{ 
+	public static ResultSet sqlAbfrage(String abfrage) {
 		Statement stmt = null;
 		ResultSet rs = null;
-		
-		try {stmt=con.createStatement();}
-		catch(Exception e){System.out.println(e.toString());}
-		
-		try 
-		{
+
+		try {
+			stmt = con.createStatement();
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+
+		try {
 			rs = stmt.executeQuery(abfrage);
-		} 
-		catch (Exception e) 
-		{
+		} catch (Exception e) {
 			System.out.println("select " + e.toString());
 		}
 
-    return rs;
+		return rs;
 	}
-	
 
 }
--- src/startpunkt/Start.java
+++ src/startpunkt/Start.java
@@ -2,11 +2,16 @@ package startpunkt;
 
 import gui.Error_Message_Box;
 import gui.Login_View;
+import de.dhbw.java.SQL_Schnittstelle;
 
 public class Start {
+
 	public static void main(String[] args) {
 		try {
+			SQL_Schnittstelle.createConnection();
 			Login_View lf = new Login_View();
+			lf.setVisible(true);
+			lf.setLoginButtonListener(new listener.Login_Listener(lf));
 		} catch (Exception e) {
 			Error_Message_Box.errorBox("Laufzeitfehler", e.getMessage(),
 					"Start.Main");
