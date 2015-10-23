e16287fdb6786c8d313798a4a63a63d8bbdd55de
Kai <SCCplayer@gmx.de>
2015-10-23 13:39:11 +0200

    UI-Manager auf Cross-Plattform umgestellt in Startpunkt.Start

--- src/startpunkt/Start.java
+++ src/startpunkt/Start.java
@@ -2,12 +2,21 @@ package startpunkt;
 
 import gui.Error_Message_Box;
 import gui.Login_View;
+
+import javax.swing.UIManager;
+
 import de.dhbw.java.SQL_Schnittstelle;
 
 public class Start {
 
 	public static void main(String[] args) {
 		try {
+			try {
+				UIManager.setLookAndFeel(UIManager
+						.getCrossPlatformLookAndFeelClassName());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 			SQL_Schnittstelle.createConnection();
 			Login_View lf = new Login_View();
 			lf.setVisible(true);
