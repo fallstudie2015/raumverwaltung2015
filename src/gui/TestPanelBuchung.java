dac67e0e57d8068b3a8e52a3cb6fed529347a47f
Kai <SCCplayer@gmx.de>
2015-10-27 10:38:00 +0100

    Buchungstable angepasst

---
+++ src/gui/TestPanelBuchung.java
@@ -0,0 +1,24 @@
+package gui;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+import de.dhbw.java.SQL_Schnittstelle;
+
+public class TestPanelBuchung {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SQL_Schnittstelle.createConnection();
+		JFrame test = new JFrame();
+		test.getContentPane().setLayout(new BorderLayout());
+		test.getContentPane().add(new PanelBuchung(), BorderLayout.CENTER);
+		test.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		test.setSize(500, 500);
+
+		test.setVisible(true);
+
+	}
+
+}
