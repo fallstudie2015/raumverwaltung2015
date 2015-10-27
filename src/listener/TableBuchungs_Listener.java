08e73f38cfbc1e2dfbb31aecf05d6ef2810e159a
Kai <SCCplayer@gmx.de>
2015-10-27 11:05:59 +0100

    Tablelistener hinzugefügt

---
+++ src/listener/TableBuchungs_Listener.java
@@ -0,0 +1,19 @@
+package listener;
+
+import gui.PanelBuchung;
+
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+public class TableBuchungs_Listener implements ListSelectionListener {
+	PanelBuchung pb;
+
+	public TableBuchungs_Listener(PanelBuchung pb) {
+		this.pb = pb;
+	}
+
+	@Override
+	public void valueChanged(ListSelectionEvent e) {
+		pb.getDatum();
+	}
+}
