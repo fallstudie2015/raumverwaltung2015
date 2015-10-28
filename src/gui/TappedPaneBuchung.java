e53e334abe40e99b177449620e9926e0b23f0412
Kai <SCCplayer@gmx.de>
2015-10-28 21:30:04 +0100

    neue Klasse TappedPaneBuchung hinzugefügt

---
+++ src/gui/TappedPaneBuchung.java
@@ -0,0 +1,30 @@
+package gui;
+
+import javax.swing.JTabbedPane;
+
+import com.toedter.calendar.JCalendar;
+
+import de.dhbw.java.Benutzer;
+
+public class TappedPaneBuchung extends JTabbedPane {
+	private PanelBuchung pb;
+	private PanelMeineBuchung pmb;
+	JCalendar jc;
+
+	public TappedPaneBuchung(JCalendar jc) {
+		this.jc = jc;
+		pmb = new PanelMeineBuchung(jc);
+		addTab("Meine Buchungen", pmb);
+		if (Benutzer.getBenutzertyp() == 'v') {
+			pb = new PanelBuchung(jc);
+			addTab("Unbestätigte Buchungen", pb);
+		}
+	}
+
+	public void reloadTappedPane() {
+		pmb.reloadTableBuchung();
+		if (Benutzer.getBenutzertyp() == 'v') {
+			pb.reloadTableBuchung();
+		}
+	}
+}
