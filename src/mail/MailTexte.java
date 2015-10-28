45c16356e9f63059c2bb6f8328b2139029ebeccc
Florian Fay <wi14138@lehre.dhbw-stuttgart.de>
2015-10-28 14:30:08 +0100

    Mail senden bei Reservierung

--- src/mail/MailTexte.java
+++ src/mail/MailTexte.java
@@ -1,9 +1,14 @@
 package mail;
 
+import java.sql.Date;
+
 import de.dhbw.java.Buchung;
 import de.dhbw.java.SQL_Schnittstelle;
 
 public abstract class MailTexte {
+
+	public static String verwalterPostfach = new String(
+			"fallstudie2015@gmx.de");
 
 	public static String getBetreffBestaetigen(Buchung buchung) {
 
@@ -45,4 +50,19 @@ public abstract class MailTexte {
 		return antwort;
 
 	}
+
+	public static String getBetreffNeueReservierung() {
+
+		String text = new String("Neue Raumreservierung eingetroffen");
+
+		return text;
+	}
+
+	public static String getTextNeueReservierung(Date datum) {
+
+		String text = new String("Eine Neue Reservierung für den " + datum
+				+ " ist eingetroffen");
+		return text;
+	}
+
 }
