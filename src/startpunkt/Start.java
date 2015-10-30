4eb613d39c43c30e2641531d10b426b8a4382cf7
Kai <SCCplayer@gmx.de>
2015-10-30 10:02:48 +0100

    cross Plattform look and feel

--- src/startpunkt/Start.java
+++ src/startpunkt/Start.java
@@ -3,10 +3,7 @@ package startpunkt;
 import gui.Error_Message_Box;
 import gui.Login_View;
 
-import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
-
-import com.alee.laf.WebLookAndFeel;
 
 import de.dhbw.java.SQL_Schnittstelle;
 
@@ -14,22 +11,12 @@ public class Start {
 
 	public static void main(String[] args) {
 		try {
-			//////////////////////////
-			SwingUtilities.invokeLater ( new Runnable ()
-	        {
-	            public void run ()
-	            {
-	                // Install WebLaF as application L&F
-	                WebLookAndFeel.install ();
-	            }
-	        } );
-			///////////////////////
-//			try {
-//				UIManager.setLookAndFeel(UIManager
-//						.getCrossPlatformLookAndFeelClassName());
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//			}
+			try {
+				UIManager.setLookAndFeel(UIManager
+						.getCrossPlatformLookAndFeelClassName());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 			SQL_Schnittstelle.createConnection();
 			Login_View lf = new Login_View();
 			lf.setVisible(true);
