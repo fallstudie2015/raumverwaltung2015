91ddc4e6ad8f0c48a59047e9bd32c100cef92c08
Kai <SCCplayer@gmx.de>
2015-11-01 19:03:41 +0100

    Kommentar hinzugefügt

--- src/de/dhbw/java/MeineTabelle.java
+++ src/de/dhbw/java/MeineTabelle.java
@@ -1,3 +1,7 @@
+/* Programmiert von: Kai Kleefisch
+ * Programmiert für: Alle in der gui angezeigten Tabellen
+ * Beschreibung: Die Klasse kann verwendet werden, um allen in der gui angezeigten Tabellen funktionen hinzuzufügen oder zu entfernen 
+ */
 package de.dhbw.java;
 
 import javax.swing.JTable;
@@ -5,6 +9,8 @@ import javax.swing.table.DefaultTableModel;
 
 public class MeineTabelle extends JTable {
 
+	// Konstruktor bekommt das Model übergeben und gibt es weiter an
+	// Mutter-Klasse
 	public MeineTabelle(DefaultTableModel model) {
 		super(model);
 	}
