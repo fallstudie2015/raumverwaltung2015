a7b0d3d47276f1c85be05b1cfb8ec30b9b238534
Kai <SCCplayer@gmx.de>
2015-11-01 17:05:40 +0100

    KLasse für die Tabellen hinzugefügt

---
+++ src/de/dhbw/java/TabellenWerkzeug.java
@@ -0,0 +1,67 @@
+/* Programmiert von: Kai Kleefisch
+ * Programmiert für: Alle Panels mit Tabellen
+ * Beschreibung: Ausgelagerter Code der mehrfach vorgekommen ist
+ */
+package de.dhbw.java;
+
+import gui.Error_Message_Box;
+
+import java.sql.ResultSet;
+
+public abstract class TabellenWerkzeug {
+
+	// Erstellung der Spaltenbezeichnung aus einem ResultSet
+	public static String[] getStringTableHeader(ResultSet rs) {
+		int anzahlSpalten = 0;
+		try {
+			anzahlSpalten = rs.getMetaData().getColumnCount();
+		} catch (Exception e) {
+			Error_Message_Box.laufzeitfehler(e,
+					"gui.PanelBuchung.getStringTableHeader");
+		}
+
+		String[] stringTableHeader = new String[anzahlSpalten];
+		try {
+			for (int i = 1; i < anzahlSpalten + 1; i++) {
+				stringTableHeader[i - 1] = rs.getMetaData().getColumnLabel(i);
+			}
+		} catch (Exception e) {
+			Error_Message_Box.laufzeitfehler(e,
+					"gui.PanelBuchung.getStringTableHeader");
+		}
+
+		return stringTableHeader;
+	}
+
+	// Erstellung der Tabellendaten aus einem ResultSet
+	public static String[][] resultSetToTableStringArray(ResultSet rs) {
+		String[][] tableData = null;
+		int anzahlSpalten = 0;
+		int anzahlZeilen = 0;
+
+		try {
+			rs.last();
+			tableData = new String[rs.getRow()][3];
+			rs.beforeFirst();
+			if (rs.next()) {
+				rs.last();
+				anzahlSpalten = rs.getMetaData().getColumnCount();
+				anzahlZeilen = rs.getRow();
+				tableData = new String[anzahlZeilen][anzahlSpalten];
+				rs.beforeFirst();
+
+				while (rs.next()) {
+					for (int i = 1; i < anzahlSpalten + 1; i++) {
+						tableData[rs.getRow() - 1][i - 1] = rs.getString(i);
+					}
+				}
+			}
+		} catch (Exception e) {
+			Error_Message_Box.laufzeitfehler(e,
+					"gui.PanelBuchung.buchungBestellerListeToTableStringArray");
+		}
+
+		return tableData;
+	}
+
+}
